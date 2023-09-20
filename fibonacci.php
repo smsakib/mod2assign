bc24f7d19340267bce7c3d8072c04bf231a7565e
Shadman Sakib <androsakib@gmail.com>
2023-09-20 19:10:43 +0600

    Update fibonacci.php

--- fibonacci.php
+++ fibonacci.php
@@ -1,6 +1,6 @@
 <?php
-function printFibonacci($n) {
-    $fibonacci = [0, 1];
+	$n = 10;
+ $fibonacci = [0, 1];
     for ($i = 2; $i < $n; $i++) {
         $fibonacci[$i] = $fibonacci[$i - 1] + $fibonacci[$i - 2];
         if ($fibonacci[$i] > 100) {
@@ -10,8 +10,5 @@ function printFibonacci($n) {
     for ($i = 0; $i < $n; $i++) {
         echo $fibonacci[$i] . " ";
     }
-}
-
-printFibonacci(10);
 
 ?> 
